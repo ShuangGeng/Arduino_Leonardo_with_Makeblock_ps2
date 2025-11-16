01aa165ae378f842ef5482ca30605a1a962432eb
耿爽 <shuangchenglian126@gmail.com>
2025-11-16 15:40:51 +0800

    v1

---
+++ src/ps2.cpp
@@ -0,0 +1,102 @@
+#include <Arduino.h>
+#include <MePS2.h>
+
+MePS2 ps2 = MePS2(&Serial1);
+
+void setup() {
+  Serial.begin(115200);
+  ps2.begin(115200);
+}
+
+void loop() {
+ ps2.loop();
+ if (ps2.ButtonPressed(MeJOYSTICK_UP))
+ {
+   Serial.println("UP is pressed!");
+ }
+ if (ps2.ButtonPressed(MeJOYSTICK_DOWN))
+ {
+   Serial.println("DOWN is pressed!");
+ }
+ if (ps2.ButtonPressed(MeJOYSTICK_LEFT))
+ {
+   Serial.println("LEFT is pressed!");
+ }
+ if (ps2.ButtonPressed(MeJOYSTICK_RIGHT))
+ {
+   Serial.println("RIGHT is pressed!");
+ }
+ if (ps2.ButtonPressed(MeJOYSTICK_L1))
+ {
+   Serial.println("L1 is pressed!");
+ }
+ if (ps2.ButtonPressed(MeJOYSTICK_L2))
+ {
+   Serial.println("L2 is pressed!");
+ }
+ if (ps2.ButtonPressed(MeJOYSTICK_R1))
+ {
+   Serial.println("R1 is pressed!");
+ }
+ if (ps2.ButtonPressed(MeJOYSTICK_R2))
+ {
+   Serial.println("R2 is pressed!");
+ }
+ if (ps2.ButtonPressed(MeJOYSTICK_TRIANGLE))
+ {
+   Serial.println("TRIANGLE is pressed!");
+ }
+ if (ps2.ButtonPressed(MeJOYSTICK_XSHAPED))
+ {
+   Serial.println("XSHAPED is pressed!");
+ }
+ if (ps2.ButtonPressed(MeJOYSTICK_SQUARE))
+ {
+   Serial.println("SQUARE is pressed!");
+ }
+ if (ps2.ButtonPressed(MeJOYSTICK_ROUND))
+ {
+   Serial.println("ROUND is pressed!");
+ }
+ if (ps2.ButtonPressed(MeJOYSTICK_START))
+ {
+   Serial.println("START is pressed!");
+ }
+ if (ps2.ButtonPressed(MeJOYSTICK_SELECT))
+ {
+   Serial.println("SELECT is pressed!");
+ }
+ if (ps2.ButtonPressed(MeJOYSTICK_MODE))
+ {
+   Serial.println("MODE is pressed!");
+ }
+ if (ps2.ButtonPressed(MeJOYSTICK_BUTTON_L))
+ {
+   Serial.println("BUTTON_L is pressed!");
+ }
+ if (ps2.ButtonPressed(MeJOYSTICK_BUTTON_R))
+ {
+   Serial.println("BUTTON_R is pressed!");
+ }
+
+ if (ps2.MeAnalog(MeJOYSTICK_RX))
+ {
+   Serial.print("MePS2_RX value is: ");
+   Serial.println(ps2.MeAnalog(MeJOYSTICK_RX), DEC);
+ }
+ if (ps2.MeAnalog(MeJOYSTICK_RY))
+ {
+   Serial.print("MePS2_RY value is: ");
+   Serial.println(ps2.MeAnalog(MeJOYSTICK_RY), DEC);
+ }
+ if (ps2.MeAnalog(MeJOYSTICK_LX))
+ {
+   Serial.print("MePS2_LX value is: ");
+   Serial.println(ps2.MeAnalog(MeJOYSTICK_LX), DEC);
+ }
+ if (ps2.MeAnalog(MeJOYSTICK_LY))
+ {
+   Serial.print("MePS2_LY value is: ");
+   Serial.println(ps2.MeAnalog(MeJOYSTICK_LY), DEC);
+ }
+}
